10a8211b663fce54d75a941011ecfd027c852f79
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-06 15:06:31 -0500

    Stretch: Added CORS, display project, styled

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const projectsRoutes = require("./projects/projects-routes.js");
 const actionsRoutes = require("./actions/actions-routes.js");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin")
+    next()
+});
+
 server.use("/projects", projectsRoutes);
 server.use("/actions", actionsRoutes);
 
--- project-api/src/Project.js
+++ project-api/src/Project.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {getProject} from './store/actions';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {ProjectDiv, ProjectDetail} from './styled-projects.js';
 
-function Project(props) {
+function Project() {
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5003/projects')
+            .then(res => {
+                console.log(res.data);
+                setProjectList(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+
     return(
-        <div>
-        <div >
-            <button onClick={() => {props.getProject()}}> Fetch the Projects </button>
-            {props.projects ? (
-                props.projects.map(project => (
-                <div className="projects" key={project.name}>
-                    <h4>Name: {project.name}</h4>
-                    <h4>Description: {project.description}</h4>
-                    <h4>Completed: {project.completed}</h4>
-                </div>
-                ))
-            ) : (
-                <h1> Projects will be here soon, please wait...</h1>
-            )}
-        </div>
-    </div>
+        <ProjectDiv>
+                {projectList.map(project => {
+                    return (<ProjectDetail key={project.id}>
+                        <h4>Project: {project.name}</h4>
+                        <h5>Description: {project.description}</h5>
+                    </ProjectDetail>)
+                })}
+        </ProjectDiv>
   );
 }
 
-const mapStateToProps = state => {
-    return{
-        projects: state.projects
-    };
-};
-
-export default connect(
-    mapStateToProps,
-    {getProject}
-) (Project);
+export default Project;
