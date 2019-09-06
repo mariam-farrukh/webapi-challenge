11659c7961c5cd3ac2aeab7e7d8b8a3f641ab884
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-06 11:31:44 -0500

    Get project by id completed

--- projects/projects-routes.js
+++ projects/projects-routes.js
@@ -4,12 +4,39 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Projects.get()
-        .then(actions => {
-            res.status(200).json(actions);
+        .then(projects => {
+            res.status(200).json(projects);
         })
         .catch(() => {
             res.status(500).json({errorMessage: "Could not get projects"});
         })
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Projects.get(id)
+        .then(project => {
+            if(project) {
+                res.status(200).json(project);
+            } else {
+                res.status(400).json({errorMessage: "This project does not exist!"})
+            }
+        })
+        .catch(() => {
+            res.status(500).json({errorMessage: "Could not get projects"});
+        })
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.delete('/', (req, res) => {
+
+});
+
+router.put('/', (req, res) => {
+    
 });
 
 module.exports = router;
